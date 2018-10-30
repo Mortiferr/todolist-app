e959c5916ad82949eee783e38a180ba7ab423f5e
Patrick Black Jr <info@patrickblackjr.com>
2018-10-29 19:40:16 -0500

    fix

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,10 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const resolvers = require('./resolvers')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-
+mongoose.connect(process.env.MONGODB_URI)
 const db = mongoose.connection
-
 db.on("error", console.error.bind(console, "connection error"))
-db.once("open", function (callback) {
+db.once("open", function(callback){
   console.log("Connection Succeeded")
 })
 
@@ -24,4 +22,5 @@ const options = {
   subscriptions: '/subscriptions',
   playground: '/playground',
 }
+
 server.start(options, ({ port }) => console.log(`Server is running on port ${port}`))
--- server/src/models.js
+++ server/src/models.js
@@ -3,31 +3,64 @@ const moment = require('moment')
 const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId
 
-function buildModel(name, schema) {
-  return mongoose.model(name, new Schema(schema, {
-    timestamps: true
-  }))
+function buildModel(name, schema, options={}) {
+  return mongoose.model(name, new Schema(schema, Object.assign({timestamps: true}, options)))
 }
+
 const Folder = buildModel('Folder', {
   name: String,
   description: String,
   shareWith: [{
     kind: String,
-    item: {
-      type: ObjectId,
-      refPath: 'shareWith.kind'
-    }
+    item: { type: ObjectId, refPath: 'shareWith.kind' }
   }],
-  parent: {
-    type: ObjectId,
-    ref: 'Folder'
-  },
+  parent: { type: ObjectId, ref: 'Folder' },
 })
 module.exports.Folder = Folder
 
-module.exports.Team = Folder.discriminator('Team', new Schema({}, {
-  timestamps: true
-}))
+module.exports.Team = Folder.discriminator('Team', new Schema({
+}, {timestamps: true}))
+
+module.exports.Task = buildModel('Task', {
+  folders: [{ type: ObjectId, ref: 'Folder' }],
+  parent: { type: ObjectId, ref: 'Task' },
+  assignees: [{ type: ObjectId, ref: 'User' }],
+  name: String,
+  description: {
+    type: String,
+    default: ''
+  },
+  creator: { type: ObjectId, ref: 'User' },
+  startDate: {
+    type: Date,
+  },
+  finishDate: {
+    type: Date,
+  },
+  duration: {
+    type: Number
+  },
+  status: {
+    type: String,
+    default: 'New'
+  },
+})
+
+module.exports.Group = buildModel('Group', {
+  team: { type: ObjectId, ref: 'Team' },
+  name: String,
+  initials: String,
+  avatarColor: String,
+  users: [{ type: ObjectId, ref: 'User' }],
+})
+
+module.exports.Record = buildModel('Record', {
+  user: { type: ObjectId, ref: 'User' },
+  task: { type: ObjectId, ref: 'Task' },
+  date: Date,
+  timeSpent: String,
+  comment: String
+})
 
 module.exports.User = buildModel('User', {
   name: {
@@ -48,10 +81,9 @@ module.exports.User = buildModel('User', {
     default: ''
   },
   avatarColor: String,
-  team: {
-    type: ObjectId,
-    ref: 'Team'
-  },
+  team: { type: ObjectId, ref: 'Team' },
   role: String,
+  rate: Number,
+  rateType: String,
   status: String
 })
